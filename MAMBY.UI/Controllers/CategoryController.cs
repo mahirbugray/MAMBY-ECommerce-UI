2a140f7e2925291f9443ec7ff2ce21923fd7fe3e
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2023-12-20 23:31:16 +0300

    categoryanasayfa

---
+++ MAMBY.UI/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MAMBY.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+
+    }
+}
---
+++ MAMBY.UI/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using MAMBY.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace MAMBY.UI.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryViewComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetFromJsonAsync<List<CategoryViewModel>>("https://localhost:7266/api/Category/GetAllCategory");
+            if (result != null)
+            {
+                return View(result);
+            }
+            return View(new List<CategoryViewModel>());
+        }
+    }
+}
