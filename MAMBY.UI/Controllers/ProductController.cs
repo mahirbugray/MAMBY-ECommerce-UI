b1953f6ca552856c1eb9bdd9eb1fc9fa24427db1
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2023-12-24 00:35:49 +0300

    update

--- MAMBY.UI/Controllers/ProductController.cs
+++ MAMBY.UI/Controllers/ProductController.cs
@@ -13,11 +13,33 @@ namespace MAMBY.UI.Controllers
 		{
 			_httpClientFactory = httpClientFactory;
 		}
-
-		public IActionResult Index()
+        [HttpGet]
+		public async Task<IActionResult> Index(int id)
         {
-            return View();
-        }
+            var client = _httpClientFactory.CreateClient();
+            if(id == 0)
+            {
+                var result = await client.GetAsync("https://localhost:7266/api/Product/GetAllProducts");
+                var jsonData = await result.Content.ReadAsStringAsync();
+                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var data = JsonConvert.DeserializeObject<List<ProductViewModel>>(jsonData);
+                    return View(data);
+                }
+            }
+            else
+            {
+                var result = await client.GetAsync("https://localhost:7266/api/Product/GetProductsByCategory/" + id);
+                var jsonData = await result.Content.ReadAsStringAsync();
+                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var data = JsonConvert.DeserializeObject<List<ProductViewModel>>(jsonData);
+                    return View(data);
+                }
+            }
+           
+			return RedirectToAction("Index", "Home");
+		}
         [HttpGet] 
         public async Task<IActionResult> Details(int id) 
         {
