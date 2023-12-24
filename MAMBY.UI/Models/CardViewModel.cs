24dcc716841e50489d622f13c556f683a77321c6
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2023-12-25 02:32:40 +0300

    edit

---
+++ MAMBY.UI/Models/CardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MAMBY.UI.Models
+{
+    public class CardViewModel
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
