8c073e4be614df723e31bc30258fb268867d3f62
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2023-12-25 21:38:16 +0300

    update edildi

--- MAMBY.UI/Controllers/CardController.cs
+++ MAMBY.UI/Controllers/CardController.cs
@@ -75,20 +75,3 @@ namespace MAMBY.UI.Controllers
 
 //Sepeteekle metoduna sessiondaki user a bakılacak ona göre ekleme yapılacak user yoksa logine yönlendirme yap.
 
-
-
-
-//[HttpPost]
-//public async Task<int> CreateCard(CardViewModel model) 
-//{
-//    var jsonData = JsonConvert.SerializeObject(model);
-//    var client = _httpClientFactory.CreateClient();
-//    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-//    var result = await client.PostAsync("https://localhost:7266/api/Card/CreateCart", content);
-//    var error = await result.Content.ReadAsStringAsync();
-//    if (result.IsSuccessStatusCode)
-//    {
-//        return model.
-//    }
-//    ModelState.AddModelError("Error", error);
-//}
