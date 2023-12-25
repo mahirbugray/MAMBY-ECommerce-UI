247353a7d38bf98decb8f1607c5c0b7327675dd9
Murat Akpınar <mrtakpnr@icloud.com>
2023-12-25 22:29:08 +0300

    isAuth "tamam"

--- MAMBY.UI/Models/CardLineViewModel.cs
+++ MAMBY.UI/Models/CardLineViewModel.cs
@@ -6,39 +6,5 @@
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal ProductPrice { get; set; }
-
-        public List<CardLineViewModel> AddCard (List<CardLineViewModel> card, CardLineViewModel cardLine)
-        {
-            if (card.Any(cd => cd.ProductId == cardLine.ProductId))
-            {
-                foreach (CardLineViewModel model in card)
-                {
-                    if(model.ProductId == cardLine.ProductId) 
-                    {
-                        model.Quantity += cardLine.Quantity;
-                    }
-                }
-            }
-            else
-            {
-                card.Add(cardLine);
-            }
-            return card;
-        }
-        public List<CardLineViewModel> DeleteCard(List<CardLineViewModel> card, int id) 
-        {
-            card.RemoveAll(cd => cd.ProductId == id);
-            return card;
-        }
-        public int TotalQuantity(List<CardLineViewModel> card)
-        {
-            var totalQuantity = card.Sum(cd => cd.Quantity);
-            return totalQuantity;
-        }
-        public decimal TotalPrice(List<CardLineViewModel> card)
-        {
-            var totalPrice = card.Sum(c => c.Quantity * c.ProductPrice);
-            return totalPrice;
-        }
     }
 }
