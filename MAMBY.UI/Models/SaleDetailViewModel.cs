627d9ace1b00a47e8a37e7ed0b973b3b1dfcccd0
Murat Akpınar <mrtakpnr@icloud.com>
2023-12-28 00:23:33 +0300

    SaleController

---
+++ MAMBY.UI/Models/SaleDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MAMBY.UI.Models
+{
+    public class SaleDetailViewModel
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int ProductId { get; set; }
+        public int SaleId { get; set; }
+
+        //Relation
+        public ProductViewModel ProductViewModel { get; set; }
+        public SaleViewModel SaleViewModel { get; set; }
+    }
+}
---
+++ MAMBY.UI/Models/SaleViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MAMBY.UI.Models
+{
+    public class SaleViewModel
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }
+        public int UserId { get; set; }
+
+        //Relation
+        public List<SaleDetailViewModel> SaleDetailViewModel { get; set; }
+    }
+}
