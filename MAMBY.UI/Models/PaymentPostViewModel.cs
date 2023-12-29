71041f1263870c14c5276091016ac43cbb929f08
Murat Akpınar <mrtakpnr@icloud.com>
2023-12-29 03:55:06 +0300

    UI son

---
+++ MAMBY.UI/Models/PaymentPostViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MAMBY.UI.Models
+{
+    public class PaymentPostViewModel
+    {
+        public List<CardLineViewModel> cardLines { get; set; }
+        public decimal totalPrice { get; set; }
+        public string city { get; set; }
+        public string neighbourhood { get; set; }
+        public string aptNo { get; set; }
+        public string postCode { get; set; }
+        public string cardNo { get; set; }
+        public string cardName { get; set; }
+        public string cardMounth { get; set; }
+        public string cardYear { get; set; }
+        public string cardCvv { get; set; }
+
+    }
+}
---
+++ MAMBY.UI/Models/PaymentViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MAMBY.UI.Models
+{
+    public class PaymentViewModel
+    {
+        public UserViewModel User { get; set; }
+        public List<CardLineViewModel> cardLines { get; set; }
+        public decimal totalPrice { get; set; }
+        public string city { get; set; }
+        public string neighbourhood { get; set; }
+        public string aptNo { get; set; }
+        public string postCode { get; set; }
+
+    }
+}
