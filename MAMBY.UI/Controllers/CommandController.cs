e48886ed2b88790b3e0068bd7a50e799432f7fd3
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2023-12-30 03:17:43 +0300

    command

---
+++ MAMBY.UI/Controllers/CommandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MAMBY.UI.Controllers
+{
+    public class CommandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
