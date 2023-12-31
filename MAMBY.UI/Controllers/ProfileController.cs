0ba79f967f87d3199d60f4b6a2c728c35577a6c0
Murat Akpınar <mrtakpnr@icloud.com>
2023-12-31 16:23:31 +0300

    searchuı

--- MAMBY.UI/Controllers/ProfileController.cs
+++ MAMBY.UI/Controllers/ProfileController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MAMBY.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MAMBY.UI.Controllers
 {
-	public class ProfileController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class ProfileController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProfileController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var user = HttpContext.Session.GetString("user");
+            var data = JsonConvert.DeserializeObject<UserViewModel>(user);
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetAsync("https://localhost:7266/api/User/GetUserById/{GetByIdUser}" + id);
+
+            return View();
+        }
+    }
 }
