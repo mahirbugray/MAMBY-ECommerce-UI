822436f2a632cf2b472951de2ec15da1d221f3e7
Murat Akpınar <mrtakpnr@icloud.com>
2024-01-01 04:34:17 +0300

    profileUI

---
+++ MAMBY.UI/Models/UpdatedUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MAMBY.UI.Models
+{
+    public class UpdatedUserViewModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
