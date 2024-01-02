11763ea64be62ea4ba9cc7ae1595260118c66832
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2024-01-03 01:56:40 +0300

    updateedit

---
+++ MAMBY.UI/Controllers/Admin/AdminProductController.cs
@@ -0,0 +1,30 @@
+﻿using MAMBY.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MAMBY.UI.Controllers.Admin
+{
+    public class AdminProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetAsync("https://localhost:7266/api/Product/GetAllFilter");
+            var error = await result.Content.ReadAsStringAsync();
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonData = await result.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<ProductViewModel>>(jsonData);
+                return View(data);
+            }
+            return View("Index", "ErrorPage");
+        }
+    }
+}
